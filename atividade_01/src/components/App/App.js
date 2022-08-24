d3e2e0dd4648abe5728608582ae412d1c7160dff
Fernando Cavaccini <fernandocavaccini@gmail.com>
2022-08-24 20:30:25 -0300

    atividade_02

---
+++ atividade_01/src/components/App/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import data from './data';
+import Evento from '../Evento/Evento';
+import Ingressos from '../Ingressos/Ingressos';
+import dataTicket from './dataTicket';
+
+function App() {
+  return (
+    
+     <div className="container">
+    <>
+    
+      <div className="wrapper">
+      {data.map(evento => (
+        <Evento
+          name={evento.name}
+          data={evento.data}
+          horario={evento.horario}
+          local={evento.local}
+          cidade={evento.cidade}
+          genero={evento.genero} />
+      ))}
+    </div>
+    <div className="wrapper">
+      {dataTicket.map(ingressos => (
+        <Ingressos
+          key={ingressos.title}
+          title={ingressos.title}
+          camarote={ingressos.camarote}
+          camaroteOpen={ingressos.camaroteOpen}
+          frontStage={ingressos.frontStage}
+          pista={ingressos.pista} />
+      ))}
+        </div></>
+      </div>
+  );
+}
+
+export default App;
---
+++ atividade_01/src/components/App/dataTicket.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    title: 'Ingressos',
+    camarote: 'R$150,00',
+    camaroteOpen: 'R$100,00',
+    frontStage: 'R$60,00',
+    pista: 'R$30,00'
+  }
+]
